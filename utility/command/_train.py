56dbed0f7ae20e065028fc922973d30f68ab8b4e
Belhadi Lahcene <lahcene.belhadi@gmail.com>
2019-08-19 19:46:27 +0200

    add _train class to generate team to fight

--- utility/command/_train.py
+++ utility/command/_train.py
@@ -15,5 +15,25 @@ import asyncio
 class Train:
     """
     Manages the utils for the train command.
+
+    - Parameter :
+
+    `caller_team` : Represents the caller team.
     """
+
+    # attribute
+    def __init__(self, caller_team):
+        self.caller_team = caller_team
+    
+    # method
+    async def generate_opponent_team(self):
+        """
+        `coroutine`
+
+        Generates a "balanced" team to oppose the caller's team.
+
+        --
+
+        Return : list of characters representing the generated team.
+        """
     pass
