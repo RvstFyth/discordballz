887627eae8202b9e9ac9dc546ba33809b0388a95
Belhadi Lahcene <lahcene.belhadi@gmail.com>
2019-08-22 19:05:49 +0200

    add get_from_unique method for Character_getter

--- utility/cog/character/getter.py
+++ utility/cog/character/getter.py
@@ -5,11 +5,14 @@ Manages the character getter.
 
 Author : DrLarck
 
-Last update : 19/08/19 (DrLarck)
+Last update : 22/08/19 (DrLarck)
 """
 
 # dependancies
 import asyncio
+
+# database
+from utility.database.database_manager import Database
 
 # getter
 class Character_getter:
@@ -27,6 +30,8 @@ class Character_getter:
     :coro:`get_character(character_id)` : Returns an instance of a character. The character id must be type int. Return None if not found.
 
     :coro:`get_summonable()` : Returns the list of summonable characters (id).
+
+    :coro:`get_from_unique(unique_id)` : Get a character instance from its unique id
     """
 
     # attribute
@@ -85,3 +90,43 @@ class Character_getter:
                 pass
 
         return(summonable)
+    
+    async def get_from_unique(self, client, unique_id):
+        """
+        `coroutine`
+
+        Get a character instance from its unique id.
+
+        - Parameter : 
+
+        `client` : Represents a `discord.Client`. The client must contain a connection pool to the database.
+
+        `unique_id` : str - Represents the character's unique id to look for.
+
+        --
+
+        Return : character instance. None if not found.
+        """
+
+        # init
+        db = Database(client.db)
+        character = None
+        get_father = await db.fetchval(
+            f"SELECT character_global_id FROM character_unique WHERE character_unique_id = '{unique_id}';"
+        )
+
+        # get the character instance
+        character = await self.get_character(get_father)
+
+        return(character)
+    
+    async def get_character_stat(self, client, unique_id):
+        """
+        `coroutine`
+
+        Returns a character instance with the stats from the db
+
+        --
+
+        Return : new character instance with the correct stats.
+        """
