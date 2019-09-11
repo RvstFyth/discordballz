00d4b63928b7c22eb93f720ec04aae84ee22b27f
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2019-09-11 16:31:45 +0100

    fix fighter team unique id display

--- cog/command/fighter.py
+++ cog/command/fighter.py
@@ -5,7 +5,7 @@ Allows the player to manage his team
 
 Author : DrLarck
 
-Last update : 08/09/19 (DrLarck)
+Last update : 11/09/19 (DrLarck)
 """
 
 # dependancies
@@ -106,7 +106,7 @@ class Cmd_fighter(commands.Cog):
         
         else:
             char_b = await self.getter.get_from_unique(self.client, player_team["b"])
-            char_b = f"{player_team['b']}` | {char_b.image.icon}{char_b.info.name} {char_b.type.icon}{char_b.rarity.icon} lv.{char_b.level:,}" 
+            char_b = f"`{player_team['b']}` | {char_b.image.icon}{char_b.info.name} {char_b.type.icon}{char_b.rarity.icon} lv.{char_b.level:,}" 
         
         ### c
         if(player_team["c"] == None):
@@ -114,7 +114,7 @@ class Cmd_fighter(commands.Cog):
         
         else:
             char_c = await self.getter.get_from_unique(self.client, player_team["c"])
-            char_c = f"{player_team['c']}` | {char_c.image.icon}{char_c.info.name} {char_c.type.icon}{char_c.rarity.icon} lv.{char_c.level:,}"
+            char_c = f"`{player_team['c']}` | {char_c.image.icon}{char_c.info.name} {char_c.type.icon}{char_c.rarity.icon} lv.{char_c.level:,}"
 
         # set display
         display_infos = f"""
