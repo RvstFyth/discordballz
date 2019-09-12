a75e618d5e1e4bc19fcb123f316bbce8c220af18
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2019-09-12 13:03:22 +0100

    finish the acid explosion ability

--- utility/cog/character/ability/list/acid_explosion.py
+++ utility/cog/character/ability/list/acid_explosion.py
@@ -5,14 +5,18 @@ Manages the Acid Explosion ability.
 
 Author : DrLarck
 
-Last update : 11/09/19 (DrLarck)
+Last update : 12/09/19 (DrLarck)
 """
 
 # dependancies
 import asyncio
+from random import randint
 
 # util
 from utility.cog.character.ability.ability import Ability
+from utility.cog.fight_system.calculator.damage import Damage_calculator
+from utility.cog.character.ability.util.effect_checker import Effect_checker
+from utility.cog.displayer.move import Move_displayer
 
 # acid explosion
 class Acid_explosion(Ability):
@@ -56,4 +60,61 @@ class Acid_explosion(Ability):
         Return : str
         """
 
+        # init
+        move = Move_displayer()
+        effect_checker = Effect_checker(self.target)
+        damage_calculator = Damage_calculator(self.caster, self.target)
+        acid_ref = await effect_checker.get_effect(
+            1,
+            self.client,
+            self.ctx,
+            self.target,
+            self.team_a,
+            self.team_b
+        )
+
+        # set the damage
+        damage = randint(self.caster.damage.ki_min, self.caster.damage.ki_max)
+        damage /= 2  # 50 % of the ki damage
+        damage = await damage_calculator.ki_damage(
+            damage,
+            critable = True
+        )
+
+        # check if the target has acid
+        has_acid = await effect_checker.get_debuff(acid_ref)
+
+        if(has_acid != None):  # the target has an acid dot active on it
+            # now check if the target has at least 3 stacks of acid
+            if(has_acid.stack >= 3):
+                # spread the dot onto the enemy members
+                
+                for unit in self.team_b:
+                    await asyncio.sleep(0)
+
+                    # init
+                    effect_checker = Effect_checker(unit)
+
+                    # check if the unit has acid stack
+                    unit_has_acid = await effect_checker.get_debuff(acid_ref)
+
+                    if(unit_has_acid != None):
+                        await unit_has_acid.add_stack()
+                        unit_has_acid.duration = unit_has_acid.initial_duration
+                    
+                    else:  # the unit doesn't have acid active on it
+                        await acid_ref.add_stack()
         
+        # setting up the move display
+        _move = await move.get_new_move()
+
+        _move["name"] = self.name
+        _move["icon"] = self.icon
+        _move["damage"] = damage["calculated"]
+        _move["critical"] = damage["critical"]
+        _move["dodge"] = damage["dodge"]
+        _move["ki"] = True
+
+        _move = await move.offensive_move(_move)
+
+        return(_move)
