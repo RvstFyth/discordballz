6c2f098cdca31b2b12f1983ffbb36048c5573be2
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2019-09-15 17:17:40 +0100

    add spreading acid ability

---
+++ utility/cog/character/ability/list/spreading_acid.py
@@ -0,0 +1,109 @@
+"""
+Manages the spreading acid ability.
+
+--
+
+Author : DrLarck
+
+Last update : 15/09/19 (DrLarck)
+"""
+
+# dependancies
+import asyncio
+
+# util
+from utility.cog.character.ability.ability import Ability
+from utility.cog.displayer.move import Move_displayer
+from utility.cog.character.ability.util.effect_checker import Effect_checker
+
+# spreading acid
+class Spreading_acid(Ability):
+    """
+    Add a stack of acid to all the characters that have at least one stack of acid active on it.
+    """
+
+    # attribute
+    def __init__(self, client, ctx, caster, target, team_a, team_b):
+        # inheritance
+        Ability.__init__(
+            self,
+            client,
+            ctx,
+            caster,
+            target,
+            team_a,
+            team_b
+        )
+
+        # ability
+        self.name = "Spreading acid"
+        self.cost = 30
+
+        # targetting
+        self.need_target = True
+        self.target_enemy = True
+    
+    # method
+    async def use(self):
+        """
+        `coroutine`
+
+        Add a stack to all active acid debuff on the enemy team.
+
+        --
+
+        Return : str
+        """
+
+        # init
+        move = Move_displayer()
+        effect_checker = Effect_checker(self.target)
+        has_unity = False
+
+        acid_ref = await effect_checker.get_effect(
+            1,
+            self.client,
+            self.ctx,
+            self.target,
+            self.team_a,
+            self.team_b
+        )
+
+        unity_ref = await effect_checker.get_effect(
+            2,
+            self.client,
+            self.ctx,
+            self.target,
+            self.team_a,
+            self.team_b
+        )
+
+        # check if there is a unity buff in the team a
+        for ally in self.team_a:
+            await asyncio.sleep(0)
+
+            unity_checker = Effect_checker(ally)
+            unity = await unity_checker.get_buff(unity_ref)
+
+            if(unity != None):
+                has_unity = True
+                break
+        
+        # now increase the stack of acids
+        for enemy in self.team_b:
+            await asyncio.sleep(0)
+
+            acid_checker = Effect_checker(enemy)
+            acid = await acid_checker.get_debuff(acid_ref)
+
+            if(acid != None):
+                await acid.add_stack()
+
+        # set up the move display
+        _move = await move.get_new_move()
+
+        _move["name"] = self.name
+        _move["icon"] = self.icon
+        _move["ki"] = True
+
+        return(_move)
