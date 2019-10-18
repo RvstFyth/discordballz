49455acd83cbaa92d5741071fb6bd42a03b839d8
Belhadi Lahcene <lahcene.belhadi@gmail.com>
2019-10-18 13:28:28 +0200

    set new version display

--- configuration/bot.py
+++ configuration/bot.py
@@ -5,7 +5,7 @@ Here is the basic configuration of the bot.
 
 Author : DrLarck
 
-Last update : 28/09/19 (DrLarck)
+Last update : 18/10/19 (DrLarck)
 """
 
 # dependancies
@@ -52,7 +52,7 @@ class Bot_config:
     is_ready = False
 
     # info
-    version = "3.0.1.87"
+    version = "3.0.1.91"
     phase = "BETA"
 
     # droprate
