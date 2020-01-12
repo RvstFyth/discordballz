cc801319be151147659bf05daa32aabdb4a03e82
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-01-12 12:28:42 +0000

    edit activity

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Project started : 06/05/19 - 8:20 PM France
 
 Author : DrLarck
 
-Last update : 28/08/19 (DrLarck)
+Last update : 12/01/2020 (DrLarck)
 """
 
 # dependancies
@@ -25,7 +25,7 @@ from utility.database.operation.database_table import Table_creator
 from configuration.bot import Bot_config
 
 # presence
-presence_activity = discord.Game(f"v{Bot_config.version} - {Bot_config.phase}")
+presence_activity = discord.Game(f"d!help | v{Bot_config.version} - {Bot_config.phase}")
 
 # init client
 client = commands.AutoShardedBot(command_prefix = Bot_config.prefix, help_command = None, activity = presence_activity)
