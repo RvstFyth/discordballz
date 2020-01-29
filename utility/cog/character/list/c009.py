b0c43f6d5aa482161e366c226233c4b770bf8656
Zyorhist <49216370+HaploZyorhist@users.noreply.github.com>
2020-01-28 19:59:00 -0700

    skeletons added
    Added skeletons for turles(9) and launch(10)

---
+++ utility/cog/character/list/c009.py
@@ -0,0 +1,41 @@
+"""
+Represents Turles
+
+--
+
+Author : Zyorhist
+
+Last update : 28/01/20 (DrLarck)
+"""
+
+# dependancies
+import asyncio
+
+# util
+from utility.cog.character.character import Character
+
+class Character_009(Character):
+    """
+    Represents the Turles unit from the Tree of Might saga.
+    """
+
+    def __init__(self):
+        Character.__init__(self)
+
+        # info
+        self.info.name = "Turles"
+        self.info.id = 9
+        self.info.saga = "Tree of Might"
+
+        # stat
+        self.health = 3675
+        
+        self.damage.physical_max = 550
+        self.damage.ki_max = 725
+
+        self.defense.armor = 550
+        self.defense.spirit = 400
+        self.defense.dodge = 10
+
+        self.critical_chance = 10
+        self.regeneration.ki = 2
